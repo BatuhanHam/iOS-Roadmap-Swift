19119d7fc2825a41fbee3a9bc28c7f5ec539b828
Batuhan Ham <batu@Batuhan-MacBook-Air-2.local>
2026-01-30 20:03:43 +0300

    The first projects were added.

---
+++ IamRich/IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Batuhan Ham on 25.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
